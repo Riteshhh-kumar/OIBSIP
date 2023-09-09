2bb53d2790ec1d21ad331754bdbe576dafa595b6
Ritesh Kumar <riteshsingh7457@gmail.com>
2023-09-09 17:35:09 +0530

    Completed

--- Task_2/src/GuessTheNumber.java
+++ Task_2/src/GuessTheNumber.java
@@ -5,7 +5,7 @@ class Game{
     static int numberOfGuess=0,score=100,attempts=5;
 
     public static void addAttempts(){
-        attempts+=5;
+        attempts=5;
     }
     static Scanner sc=new Scanner(System.in);
     Game(int num){
@@ -31,7 +31,22 @@ class Game{
                 }
                 if(answer){
                     System.out.println("Sorry you were not able to guess the number in 5 attempts.");
+                    System.out.println("want another 5 attempts?If yes enter y");
+                    char choose = sc.next().charAt(0);
+
+                    switch (choose){
+                        case 'y', 'Y':{
+                            Game.addAttempts();
+                            System.out.println("Please take a guess!");
+                            Game.gameMode(1,num);
+                            break;
+                        }
+                        default:{
+                            System.out.println("Thank you for playing number guessing Game.");
+                        }
+                    }
                 }
+
                 break;
 
             }
@@ -39,7 +54,10 @@ class Game{
             case 2:
             {
                 while(Game.takeUserInput(num,attempts))
-                Game.takeUserInput(num,attempts);
+                    Game.takeUserInput(num,attempts);
+                System.out.println("Thank you for playing number guessing Game.");
+
+
                 break;
 
             }
@@ -58,7 +76,7 @@ class Game{
                 return false;
 
             if(attempts!=0)
-            System.out.println("Please take another guess!");
+                System.out.println("Please take another guess!");
             return true;
 
 
@@ -67,18 +85,18 @@ class Game{
         if(input==num && attempts>0)
         {
             numberOfGuess++;
-            System.out.println("You guessed the correct number in " +numberOfGuess+" number of guesses and got a score of "+score);
+            System.out.println("You guessed the correct number in " +numberOfGuess+" number of guesses and got a score of "+score+"/100");
             return false;
         }
         else if(input<num && attempts>0){
             System.out.println("Guessed number is small ! Please guess a bigger number");
             numberOfGuess++;
-            score-=2;
+            score-=5;
         }
         else if(input>num && attempts>0){
             System.out.println("Guessed number is big ! Please guess a smaller number");
             numberOfGuess++;
-            score-=2;
+            score-=5;
         }
         return true;
 
